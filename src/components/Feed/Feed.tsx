442ed2f45ee275ef0d563ebd71288d4513f178f8
Pavel Mezencev <mezencev24@mail.ru>
2023-10-25 10:03:45 +0300

    refactor with react firebase hooks

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,42 +1,24 @@
-import cls from './Feed.module.css';
 import CreateIcon from '@mui/icons-material/Create';
-import { inputOptions } from './config/inputOptions';
-import InputOptions from './InputOptions';
-import Post from './Post';
-import {
-  ChangeEvent,
-  useCallback,
-  useState,
-  useEffect,
-  FormEvent,
-} from 'react';
-import { IPost, PostData } from 'types/post';
 import {
   addDoc,
   collection,
-  onSnapshot,
   orderBy,
   query,
   serverTimestamp,
 } from 'firebase/firestore';
+import { ChangeEvent, FormEvent, useCallback, useState } from 'react';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { IPost } from 'types/post';
 import { db } from '../../../firebase.config';
+import cls from './Feed.module.css';
+import InputOptions from './InputOptions';
+import Post from './Post';
+import { inputOptions } from './config/inputOptions';
 
 const Feed = () => {
   const [input, setInput] = useState('');
-  const [posts, setPosts] = useState<PostData[]>([]);
-
-  useEffect(() => {
-    const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      const postsData: PostData[] = [];
-      querySnapshot.forEach((doc) => {
-        postsData.push({ id: doc.id, data: doc.data() as IPost });
-      });
-      setPosts(postsData);
-
-      return () => unsubscribe();
-    });
-  }, []);
+  const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
+  const [value, loading] = useCollection(q);
 
   const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
@@ -56,6 +38,8 @@ const Feed = () => {
     [input]
   );
 
+  if (loading) return <h1>Loading...</h1>;
+
   return (
     <div className={cls.feed}>
       <div className={cls.inputContainer}>
@@ -72,8 +56,8 @@ const Feed = () => {
           ))}
         </div>
       </div>
-      {posts.map((post) => (
-        <Post key={post.id} post={post.data} />
+      {value?.docs.map((post) => (
+        <Post key={post.id} post={post.data() as IPost} />
       ))}
     </div>
   );
